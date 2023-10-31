837890f3c308ab5f100cbfeb2d8401161845c466
Michael Scharkow <michael@underused.org>
2023-10-31 10:16:36 +0100

    Add image and audio classification.

--- R/hf_api.R
+++ R/hf_api.R
@@ -1,5 +1,5 @@
 #' @export
-hf_api = function(inputs, url, ...){
+hf_api = function(inputs = NULL, url = NULL, filename = NULL, ...){
   require(httr);require(jsonlite)
   hf_opts = list()
   if (Sys.getenv("HF_API_TOKEN") != "") {
@@ -7,14 +7,21 @@ hf_api = function(inputs, url, ...){
   } else {
     config = NULL
   }
+
   params = c(list(inputs = inputs), list(...))
-  body = jsonlite::toJSON(c(params, hf_opts))
+
+  if(!is.null(filename)){
+    body = read_file_raw(filename)
+  } else {
+    body = jsonlite::toJSON(c(params, hf_opts))
+  }
+
   req = POST(url, body = body, config = config)
   response = fromJSON(content(req, "text"))
   if(is.list(response) && !is.null(response$estimated_time)){
     print(paste("Waiting", round(as.numeric(response$estimated_time)), "seconds for model to load."))
     Sys.sleep(response$estimated_time)
-    hf_api(inputs, url, ...)
+    hf_api(inputs, url, filename, ...)
   } else {
     response
   }
@@ -22,7 +29,8 @@ hf_api = function(inputs, url, ...){
 
 #' @export
 text_embeddings = function(txt, url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L12-v2"){
-  hf_api(inputs = txt, url = url)
+  hf_api(inputs = txt, url = url) %>%
+    as_tibble()
 }
 
 #' @export
@@ -49,5 +57,18 @@ text_classification = function(txt, url = "https://api-inference.huggingface.co/
 
 #' @export
 text_zeroshot = function(txt, labels, url = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"){
-  hf_api(inputs = txt, url = url, parameters = list(candidate_labels = labels))
+  hf_api(inputs = txt, url = url, parameters = list(candidate_labels = labels)) %>%
+    as_tibble()
+}
+
+#' @export
+image_classification = function(filename, url = "https://api-inference.huggingface.co/models/google/vit-base-patch16-224"){
+  hf_api(filename = filename, url = url) %>%
+    as_tibble()
+}
+
+#' @export
+audio_classification = function(filename, url = "https://api-inference.huggingface.co/models/superb/hubert-large-superb-er"){
+  hf_api(filename = filename, url = url) %>%
+    as_tibble()
 }
